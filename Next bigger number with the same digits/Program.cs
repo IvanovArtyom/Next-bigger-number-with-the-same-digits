cf67b8f1749a178fca4813d5c1ee25264f2cc04a
Артем Иванов <IvanovArtyom3000@yandex.ru>
2023-06-09 16:17:53 +0300

    Добавьте файлы проекта.

---
+++ Next bigger number with the same digits/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+public class Kata
+{
+    public static void Main()
+    {
+        // Test
+        var t = NextBiggerNumber(1234567890);
+        // ...should return 1234567908
+    }
+
+    public static long NextBiggerNumber(long n)
+    {
+        string nAsStr = n.ToString();
+
+        for (int i = nAsStr.Length - 1; i > 0; i--)
+        {
+            if (nAsStr[i] <= nAsStr[i - 1])
+                continue;
+
+            char nextNumber = nAsStr.Last(c => c > nAsStr[i - 1]);
+            var lastNumbers = nAsStr[(i - 1)..].OrderBy(c => c).ToList();
+            lastNumbers.Remove(nextNumber);
+
+            return long.Parse(string.Join("", nAsStr[0..(i - 1)], nextNumber, string.Join("", lastNumbers)));
+        }
+
+        return -1;
+    }
+}
